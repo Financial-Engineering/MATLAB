8c9bb90d40a81b2a8d5d788fb28042a455c166a1
Richard Lewis <rlewis@iMac.local>
2011-03-13 15:36:39 +1100

    latex comments

--- Volatility/Heston2/f2.m
+++ Volatility/Heston2/f2.m
@@ -1,7 +1,14 @@
 function f = f2(u,F,K,kappa,theta,omega,tau,V0,rho)
     f = 0;
     if (tau > 0) && (V0 >= 0) && abs(rho) < 1
+        %%
+        %
+        % $$ d=\sqrt{\left(\rho\omega u\i - \kappa\right)^2 + \omega^2\left(u\i+u^2\right)} $$       
         d = sqrt((rho*omega*u*1i - kappa).^2 + omega^2*(u*1i+u.^2));
+        
+        %%
+        %
+        % $$ c=\frac{\kappa-\rho\omega u \i + d}{\kappa-\rho\omega u\i - d} $$               
         c = (kappa - rho * omega * u * 1i + d) / (kappa - rho * omega * u * 1i - d);
         tc = angle(c);
         Gd = c - 1;
